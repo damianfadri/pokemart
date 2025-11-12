a4eafe7a64f4d439533aa6c7fbfa2c26c6916aff
damianfadri <55902230+damianfadri@users.noreply.github.com>
2025-11-13 00:04:04 +0800

    test: Enable Jasmine tests for Firefox (#3)

--- src/webapp/src/app/item/item.spec.ts
+++ src/webapp/src/app/item/item.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ItemComponent } from './item.component';
+import { input } from '@angular/core';
 
 describe('Item', () => {
   let component: ItemComponent;
@@ -8,16 +9,45 @@ describe('Item', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ItemComponent]
+      imports: [ItemComponent],
+
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(ItemComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('name', 'Potion');
+    fixture.componentRef.setInput('price', 300);
+    fixture.componentRef.setInput('category', 'Potions');
+    fixture.componentRef.setInput('description', 'Heals 20 HP');
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  describe('name should', () => {
+    it('be rendered', () => {
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(compiled.querySelector('[data-testid=item-name]')?.textContent).toContain('Potion');
+    });
+  });
+
+  describe('price should', () => {
+    it('be rendered', () => {
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(compiled.querySelector('[data-testid=item-price')?.textContent).toContain('300');
+    });
+  });
+
+  describe('category should', () => {
+    it('be rendered', () => {
+      const compiled = fixture.nativeElement as HTMLElement;
+      expect(compiled.querySelector('[data-testid=item-category')?.textContent).toContain('Potions');
+    });
   });
 });
