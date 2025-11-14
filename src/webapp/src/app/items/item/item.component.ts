6827c10adfd73037cfdaee44e82ec7363df0c181
damianfadri <damianfadri@gmail.com>
2025-11-14 17:26:05 +0800

    add working copy

--- src/webapp/src/app/items/item/item.component.ts
+++ src/webapp/src/app/items/item/item.component.ts
@@ -1,4 +1,6 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { Item } from './item.model';
+import { CartItem } from '../../cart/cart.model';
 
 @Component({
   selector: 'app-item',
@@ -11,4 +13,14 @@ export class ItemComponent {
   category = input.required<string>();
   price = input.required<number>();
   description = input.required<string>();
+
+  addToCart = output<CartItem>();
+
+  onAdd() {
+    this.addToCart.emit({
+      name: this.name(),
+      quantity: 1,
+      price: this.price()
+    });
+  }
 }
