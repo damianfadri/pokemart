de4020b6032effa70b1868229bfbbb95733c81e6
damianfadri <damianfadri@gmail.com>
2025-11-15 14:14:44 +0800

    add filters tests

--- src/webapp/src/app/filters/filters.spec.ts
+++ src/webapp/src/app/filters/filters.spec.ts
@@ -3,21 +3,111 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FiltersComponent } from './filters';
 
 describe('Filters', () => {
-  let component: FiltersComponent;
-  let fixture: ComponentFixture<FiltersComponent>;
+  describe('toggle should', () => {
+    it('add category when val is true and not in categories', () => {
+      var fixture = 
+        new SutBuilder()
+          .withCategories([])
+          .build();
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [FiltersComponent]
-    })
-    .compileComponents();
+      fixture.componentInstance.toggle('Category1', true);
 
-    fixture = TestBed.createComponent(FiltersComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      expect([...fixture.componentInstance.categories()])
+      .toEqual(['Category1']);
+    });
+
+    it('do nothing when val is true and in categories', () => {
+      var fixture = 
+        new SutBuilder()
+          .withCategories(['Category1'])
+          .build();
+
+      fixture.componentInstance.toggle('Category1', true);
+
+      expect([...fixture.componentInstance.categories()])
+      .toEqual(['Category1']);
+    });
+
+    it('remove category when val is false and in categories', () => {
+      var fixture = 
+        new SutBuilder()
+          .withCategories(['Category1'])
+          .build();
+
+      fixture.componentInstance.toggle('Category1', false);
+
+      expect([...fixture.componentInstance.categories()])
+      .toEqual([]);
+    });
+
+    it('do nothing when val is false and not in categories', () => {
+      var fixture = 
+        new SutBuilder()
+          .withCategories(['Category2'])
+          .build();
+
+      fixture.componentInstance.toggle('Category1', false);
+
+      expect([...fixture.componentInstance.categories()])
+      .toEqual(['Category2']);
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('toggled should', () => {
+    it('return true if in categories', () => {
+      var fixture = 
+        new SutBuilder()
+          .withCategories(['Category1'])
+          .build();
+
+      expect(fixture.componentInstance.toggled('Category1')).toBeTrue();
+    });
+
+    it('return false if not in categories', () => {
+      var fixture = 
+        new SutBuilder()
+          .withCategories(['Category2'])
+          .build();
+
+      expect(fixture.componentInstance.toggled('Category1')).not.toBeTrue();
+    });
+  });
+
+  describe('onApply should', () => {
+    it('call filtersService.filters with the correct filter', () => {
+      var fixture = 
+        new SutBuilder()
+          .withCategories(['Category1'])
+          .build();
+
+      spyOn(fixture.componentInstance.filtersService.filters, 'update');
+
+      fixture.componentInstance.onApply();
+
+      expect(fixture.componentInstance.filtersService.filters.update).toHaveBeenCalled();
+    });
   });
 });
+
+class SutBuilder {
+  private categories: string[] = [];
+
+  withCategories(categories: string[]): SutBuilder {
+    this.categories = categories;
+    return this;
+  }
+
+  build(): ComponentFixture<FiltersComponent> {
+    TestBed.configureTestingModule({
+      imports: [FiltersComponent],
+    });
+    
+    const fixture = TestBed.createComponent(FiltersComponent);
+
+    fixture.componentInstance.categories.set(
+      new Set<string>(this.categories)
+    );
+
+    return fixture;
+  }
+}
