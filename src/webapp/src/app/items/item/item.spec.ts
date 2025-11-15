4ad6c77b072e9658e4b9cb18568f95d37096ea0b
damianfadri <damianfadri@gmail.com>
2025-11-15 17:01:33 +0800

    cleanup item

--- src/webapp/src/app/items/item/item.spec.ts
+++ src/webapp/src/app/items/item/item.spec.ts
@@ -3,71 +3,62 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ItemComponent } from './item';
 import { input } from '@angular/core';
 import { Item } from './item.model';
+import { CartService } from '../../cart/cart.service';
 
 describe('Item', () => {
+  let fixture: ComponentFixture<ItemComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ItemComponent],
+      providers: [CartService]
+    });
+    
+    fixture = TestBed.createComponent(ItemComponent);
+  });
+
   describe('item should', () => {
     it('be initialized', () => {
-      const fixture = new SutBuilder()
-        .withName('Potion')
-        .withPrice(300)
-        .withCategory('Healing')
-        .withDescription('Something')
-        .build();
+      fixture.componentRef.setInput('item', {
+        name: 'Potion',
+        price: 300,
+        category: 'Medicines'
+      });
 
-      fixture.detectChanges();
-
-      expect(fixture.componentInstance.item().name).toBe('Potion');
+      expect(fixture.componentInstance.item())
+        .toEqual({
+          name: 'Potion',
+          price: 300,
+          category: 'Medicines'
+        });
     });
 
-    it('render name', () => {
-      const fixture = new SutBuilder()
-        .withName('Potion')
-        .withPrice(300)
-        .withCategory('Healing')
-        .withDescription('Heals 20 HP')
-        .build();
+    it('render item', () => {
+      fixture.componentRef.setInput('item', {
+        name: 'Potion',
+        price: 300,
+        category: 'Medicines'
+      });
 
       fixture.detectChanges();
 
       const compiled = fixture.nativeElement as HTMLElement;
-      expect(compiled.querySelector('[data-testid=item-name]')?.textContent).toContain('Potion');
-    });
-
-    it('render price', () => {
-      const fixture = new SutBuilder()
-        .withName('Potion')
-        .withPrice(300)
-        .withCategory('Healing')
-        .withDescription('Heals 20 HP')
-        .build();
-
-      fixture.detectChanges();
-
-      const compiled = fixture.nativeElement as HTMLElement;
-      expect(compiled.querySelector('[data-testid=item-price')?.textContent).toContain('300');
-    });
-
-    it('render category', () => {
-      const fixture = new SutBuilder()
-        .withName('Potion')
-        .withPrice(300)
-        .withCategory('Healing')
-        .withDescription('Heals 20 HP')
-        .build();
-
-      fixture.detectChanges();
-
-      const compiled = fixture.nativeElement as HTMLElement;
-      expect(compiled.querySelector('[data-testid=item-category')?.textContent).toContain('Healing');
+      expect(compiled.querySelector('[data-testid=item-name]')?.textContent)
+        .toContain('Potion');
+      expect(compiled.querySelector('[data-testid=item-price]')?.textContent)
+        .toContain('300');
+      expect(compiled.querySelector('[data-testid=item-category]')?.textContent)
+        .toContain('Medicines');
     });
   });
 
   describe('addToCart should', () => {
-    it('call cartService.addItem with the correct item', async () => {
-      const fixture = new SutBuilder()
-        .withName('Potion')
-        .withPrice(300)
-        .build();
+    it('call cartService.addItem with the correct item', () => {
+      fixture.componentRef.setInput('item', {
+        name: 'Potion',
+        price: 300,
+        category: 'Medicines'
+      });
 
       spyOn(fixture.componentInstance.cartService, 'addItem');
 
@@ -77,62 +68,11 @@ describe('Item', () => {
         .toHaveBeenCalledWith({ 
           item: {
             name: 'Potion',
-            price: 300
+            price: 300,
+            category: 'Medicines'
           },
           quantity: 1, 
         });
     });
   });
 });
-
-class SutBuilder {
-  private name: string = 'Potion';
-  private price: number = 300;
-  private category?: string;
-  private description?: string;
-
-  withName(name: string): SutBuilder {
-    this.name = name;
-    return this;
-  }
-
-  withPrice(price: number): SutBuilder {
-    this.price = price;
-    return this;
-  }
-
-  withCategory(category: string): SutBuilder {
-    this.category = category;
-    return this;
-  }
-
-  withDescription(description: string): SutBuilder {
-    this.description = description;
-    return this;
-  }
-
-  build() : ComponentFixture<ItemComponent> {
-      TestBed.configureTestingModule({
-      imports: [ItemComponent],
-    });
-    
-    const fixture = TestBed.createComponent(ItemComponent);
-
-    const item: Item = {
-      name: this.name,
-      price: this.price,
-    };
-
-    if (this.category) {
-      item.category = this.category;
-    }
-
-    if (this.description) {
-      item.description = this.description;
-    }
-
-    fixture.componentRef.setInput('item', item);
-
-    return fixture;
-  }
-}
