d1ad9533026d32471e37265c4845201e2b9e039b
damianfadri <damianfadri@gmail.com>
2025-11-16 13:53:43 +0800

    add routing to products

--- src/webapp/src/app/app.routes.ts
+++ src/webapp/src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home';
+import { ProductComponent } from './products/product/product';
 
 export const routes: Routes = [
     {
-        path: '', 
+        path: 'products', 
         component: HomeComponent
+    },
+    {
+        path: 'products/:id',
+        component: ProductComponent
+    },
+    {
+        path: '**',
+        redirectTo: 'products'
     }
 ];
