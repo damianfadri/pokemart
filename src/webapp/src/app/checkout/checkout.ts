e746eca2cc32f2d03a2cb8eff0ec850acbc3a363
damianfadri <damianfadri@gmail.com>
2025-11-21 16:01:58 +0800

    add switch if cart is empty

--- src/webapp/src/app/checkout/checkout.ts
+++ src/webapp/src/app/checkout/checkout.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { CartContext } from '../cart/cart.context';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './checkout.html',
   styleUrl: './checkout.css',
 })
 export class CheckoutComponent {
+  cartContext = inject(CartContext);
 
+  cart = computed(() => this.cartContext.list());
+  cartEmpty = computed(() => this.cart().length === 0);
 }
